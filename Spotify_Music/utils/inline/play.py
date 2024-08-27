f65cb4afac77ba47e6215d2dcbde2fc19a187591
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-08-27 21:21:08 +0530

    Update play.py

--- Spotify_Music/utils/inline/play.py
+++ Spotify_Music/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
        [
             InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/ll_ISTKHAR_ll"),
-           InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -83,7 +83,7 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
        [
-            InlineKeyboardButton(text="ll_ISTKHAR_ll"rl="https://t.me/ll_ISTKHAR_ll"),
+            InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/ll_ISTKHAR_ll"),
         InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
